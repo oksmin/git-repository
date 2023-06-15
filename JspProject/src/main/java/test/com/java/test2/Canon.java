b9ba2dd040f473fa908b77e8495547753267f935
oksamin <oksmin@gmail.com>
2023-06-15 18:05:29 +0900

    화이팅

--- JspProject/src/main/java/test/com/java/test2/Canon.java
+++ JspProject/src/main/java/test/com/java/test2/Canon.java
@@ -4,7 +4,7 @@ public class Canon implements Printer{
 
 	@Override
 	public void print() {
-		System.out.println("cannon 프린fff터 인쇄중sdfsdf....");
+		System.out.println("cannon 프린fff터 인sdfsdf쇄중sdfsdf....");
 	}
 
 }
